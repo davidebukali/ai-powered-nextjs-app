8193a280afbba3b08b86537022827f1fdf7cf9ae
David <debukali@gmail.com>
2024-06-01 12:35:55 +0300

    Remove question component

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -27,9 +27,6 @@ const JournalPage = async () => {
   return (
     <div className="p-10 bg-zinc-400/10 h-full">
       <h2 className="text-3xl mb-8">Journal</h2>
-      <div className="p-10">
-        <Question />
-      </div>
       <div className="grid grid-cols-3 gap-4 p-10">
         <NewEntryCard />
         {entries.map((entry) => (
