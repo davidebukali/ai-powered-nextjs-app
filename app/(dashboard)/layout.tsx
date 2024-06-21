2855cff3456c470b64a4824b52c800995a38f2b9
David <debukali@gmail.com>
2024-06-21 15:33:30 +0300

    Make header responsive

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,10 @@
+'use client'
+
 import { UserButton } from '@clerk/nextjs'
 import Link from 'next/link'
+import { useState } from 'react'
+import { FaBars } from 'react-icons/fa'
+import classNames from 'classnames'
 
 const Links = [
   { href: '/', label: 'Home' },
@@ -8,26 +13,65 @@ const Links = [
 ]
 
 const DashboardLayout = ({ children }) => {
+  const [sliderState, setSliderState] = useState(true)
+  const renderLinks = () => {
+    return Links.map((link) => (
+      <li key={link.label} className="px-2 py-4 text-lg">
+        <Link href={link.href}>{link.label}</Link>
+      </li>
+    ))
+  }
+
+  const toggleSlideOver = () => {
+    setSliderState(!sliderState)
+  }
+
   return (
-    <div className="h-screen w-screen relative">
-      <aside className="absolute top-0 left-0 h-full w-[200px] border-r border-black/10">
-        <div className="px-4 my-4">
-          <span className="text-3xl">Mood</span>
-          <ul>
-            {Links.map((link) => (
-              <li key={link.label} className="px-2 py-4 text-2xl">
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </aside>
-      <div className="ml-[200px] h-full w-[calc(100vw-200px)]">
-        <header className="h-[60px] border-b border-black/10">
-          <div className="h-full px-6 flex items-center justify-end">
-            <UserButton />
+    <div className="h-screen w-screen">
+      <div className="h-full w-full">
+        <header className="h-[60px]">
+          <div className="flex">
+            <span className="text-2xl pt-4 py-2 ml-4 cursor-pointer visible sm:invisible">
+              <FaBars onClick={() => toggleSlideOver()} />
+            </span>
+            <span className="basis-1/4 text-3xl px-2 py-2 cursor-pointer">
+              Mood
+            </span>
+            <ul className="flex basis-1/2 invisible sm:visible">
+              {renderLinks()}
+            </ul>
+            <div className="basis-1/4 w-full flex px-6 items-center justify-end">
+              <UserButton />
+            </div>
           </div>
         </header>
+        <div
+          id="slideover-container"
+          className={classNames('absolute h-full w-full', {
+            invisible: sliderState,
+          })}
+        >
+          <div
+            id="sliderover-bg"
+            className={classNames(
+              'absolute duration-500 ease-out transition-all inset-0 w-full h-full bg-gray-900',
+              { 'opacity-0': sliderState },
+              { 'opacity-50': !sliderState }
+            )}
+          ></div>
+          <div
+            id="slideover"
+            className={classNames(
+              'absolute duration-500 ease-out transition-all left-0 bg-white w-[200px] h-full',
+              {
+                '-translate-x-full': sliderState,
+              }
+            )}
+          >
+            <ul className="">{renderLinks()}</ul>
+          </div>
+        </div>
+
         <div className="h-full">{children}</div>
       </div>
     </div>
