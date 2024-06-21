1d5bb4082701cae0919ec2b6cab5ae53316698e0
David <debukali@gmail.com>
2024-06-21 15:53:10 +0300

    Make journal page responsive

--- app/(dashboard)/journal/page.tsx
+++ app/(dashboard)/journal/page.tsx
@@ -26,8 +26,7 @@ const JournalPage = async () => {
 
   return (
     <div className="p-10 bg-zinc-400/10 h-full">
-      <h2 className="text-3xl mb-8">Journal</h2>
-      <div className="grid grid-cols-3 gap-4 p-10">
+      <div className="grid gap-4 p-10 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4">
         <NewEntryCard />
         {entries.map((entry) => (
           <Link href={`/journal/${entry.id}`} key={entry.id}>
