9dc42db52d2b501f2a8a39704e6d310e3d09a850
David <debukali@gmail.com>
2024-06-21 17:56:29 +0300

    Show loading spinner when creating a new entry

--- Components/NewEntryCard.tsx
+++ Components/NewEntryCard.tsx
@@ -2,10 +2,14 @@
 
 import { createNewEntry } from '@/utils/api'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { FaSpinner } from 'react-icons/fa'
 
 const NewEntryCard = () => {
   const router = useRouter()
+  const [isLoading, setIsLoading] = useState(false)
   const handleOnClick = async () => {
+    setIsLoading(true)
     const data = await createNewEntry()
     router.push(`/journal/${data.id}`)
   }
@@ -15,6 +19,12 @@ const NewEntryCard = () => {
       className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
       onClick={handleOnClick}
     >
+      {isLoading && (
+        <div className="flex px-4 sm:p-2">
+          <FaSpinner className="animate-spin" />
+          <span className="px-2">Loading ...</span>
+        </div>
+      )}
       <div className="px-4 py-5 sm:p-6">
         <span className="text-3xl">New Entry</span>
       </div>
