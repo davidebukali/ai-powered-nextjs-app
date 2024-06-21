d296a34a576f69cf3d33edb53770076c18e06c73
David <debukali@gmail.com>
2024-06-21 15:37:40 +0300

    Update homepage styles and text

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ export default async function Home() {
   let href = userId ? '/journal' : '/new-user'
   return (
     <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[600px] mx-auto">
+      <div className="w-full max-w-[600px] mx-auto p-10">
         <h1 className="text-6xl mb-4">The best journal app</h1>
         <p className="text-2xl text-white/60 mb-4">
-          Best app for tracking your mood
+          Mood App uses AI to track your mood
         </p>
         <div>
           <Link href={href}>
