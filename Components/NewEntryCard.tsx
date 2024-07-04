c6388f09f58a31eab737eaf4bdf58ab4741578b0
David <debukali@gmail.com>
2024-07-04 06:06:56 +0300

    Use link package to navigate to create entry page

--- Components/NewEntryCard.tsx
+++ Components/NewEntryCard.tsx
@@ -1,32 +1,16 @@
-'use client'
-
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { FaSpinner } from 'react-icons/fa'
+import Link from 'next/link'
+import { FaPlus } from 'react-icons/fa'
 
 const NewEntryCard = () => {
-  const router = useRouter()
-  const [isLoading, setIsLoading] = useState(false)
-  const handleOnClick = async () => {
-    setIsLoading(true)
-    router.push('/journal/add')
-  }
-
   return (
-    <div
-      className="cursor-pointer overflow-hidden rounded-lg bg-white shadow"
-      onClick={handleOnClick}
-    >
-      {isLoading && (
-        <div className="flex px-4 sm:p-2">
-          <FaSpinner className="animate-spin" />
-          <span className="px-2">Loading ...</span>
+    <Link href="/journal/add">
+      <div className="cursor-pointer overflow-hidden rounded-lg bg-white shadow">
+        <div className="px-4 py-5 sm:p-6 flex">
+          <FaPlus className="text-xl mx-2 mt-2" />
+          <span className="text-3xl">New Entry</span>
         </div>
-      )}
-      <div className="px-4 py-5 sm:p-6">
-        <span className="text-3xl">New Entry</span>
       </div>
-    </div>
+    </Link>
   )
 }
 
