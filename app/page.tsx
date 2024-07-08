90834680ff16b3bd72996f13d595355a0629d459
David <debukali@gmail.com>
2024-07-08 13:21:50 +0300

    Add geometry to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,83 @@
-import { auth } from '@clerk/nextjs/server'
-import Link from 'next/link'
+'use client'
 
-export default async function Home() {
-  const { userId } = await auth()
-  let href = userId ? '/journal' : '/new-user'
+import Link from 'next/link'
+import * as THREE from 'three'
+import { memo, useEffect, useRef } from 'react'
+import { useAuth } from '@clerk/clerk-react'
+
+const Home = memo(function HomePage() {
+  const { getToken, isLoaded, isSignedIn } = useAuth()
+  const renderTarget = useRef(null)
+  useEffect(() => {
+    let continueRendering = true
+    const scene = new THREE.Scene()
+    scene.background = new THREE.Color(0xffffff)
+    const camera = new THREE.PerspectiveCamera(
+      75,
+      renderTarget.current.clientWidth / renderTarget.current.clientHeight,
+      0.1,
+      1000
+    )
+    const renderer = new THREE.WebGLRenderer()
+    renderer.setSize(
+      renderTarget.current.clientWidth,
+      renderTarget.current.clientHeight
+    )
+    renderTarget.current.replaceChildren(renderer.domElement)
+    const geometry = new THREE.TorusKnotGeometry(50, 20, 50, 16)
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x049ef4,
+      wireframe: true,
+    })
+    const torusKnot = new THREE.Mesh(geometry, material)
+    scene.add(torusKnot)
+    const directionalLight = new THREE.DirectionalLight(0x000000, 0.5)
+    scene.add(directionalLight)
+    const light = new THREE.AmbientLight(0x404040)
+    scene.add(light)
+    camera.position.z = 200
+    function animate() {
+      if (continueRendering) {
+        requestAnimationFrame(animate)
+      }
+      torusKnot.rotation.x += 0.01
+      torusKnot.rotation.y += 0.01
+      renderer.render(scene, camera)
+    }
+    animate()
+    return () => {
+      continueRendering = false
+    }
+  }, [])
+
+  let href = '/'
+  if (isSignedIn) {
+    href = '/journal'
+  } else {
+    href = '/new-user'
+  }
+
   return (
-    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
-      <div className="w-full max-w-[600px] mx-auto p-10">
-        <h1 className="text-6xl mb-4">The best journal app</h1>
-        <p className="text-2xl text-white/60 mb-4">
-          Mood App uses AI to track how you are feeling by analysing your daily
-          journal entries.
-        </p>
-        <div>
-          <Link href={href}>
-            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl">
-              Get started
-            </button>
-          </Link>
+    <div className="w-screen h-screen grid gap-2 md:grid-cols-2 sm:grid-cols-1">
+      <div className="flex justify-center items-center">
+        <div className="p-10">
+          <h1 className="text-6xl mb-4 text-black">Mood</h1>
+          <p className="text-2xl text-black/60 mb-4">
+            A journal writing app that uses AI to track how you are feeling by
+            analysing your daily entries.
+          </p>
+          <div>
+            <Link href={href}>
+              <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl text-white">
+                Get started
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
+      <div ref={renderTarget}></div>
     </div>
   )
-}
+})
+
+export default Home
