50581b49299140eb7e62431a2b3c486ff350f079
David <debukali@gmail.com>
2024-07-09 02:17:02 +0300

    Use react fibre to render animation

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,83 @@
 'use client'
 
 import Link from 'next/link'
-import * as THREE from 'three'
-import { memo, useEffect, useRef } from 'react'
+import { memo, useRef, useState } from 'react'
 import { useAuth } from '@clerk/clerk-react'
+import classNames from 'classnames'
+import { requestAnalysis } from '@/utils/api'
+import { FaSpinner } from 'react-icons/fa'
+import { Object3D } from 'three'
+import { Canvas, useFrame, extend, useThree } from '@react-three/fiber'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+const dummy = new Object3D()
+extend({ OrbitControls })
+
+const Cubes = ({ color }) => {
+  const meshRef = useRef(null)
+
+  useFrame(({ clock }) => {
+    const time = clock.getElapsedTime()
+    meshRef.current.rotation.x = Math.sin(time / 4)
+    meshRef.current.rotation.y = Math.sin(time / 4)
+    let i = 0
+    const amount = 4
+    const offset = (amount - 1) / 2
+
+    for (let x = 0; x < amount; x++) {
+      for (let y = 0; y < amount; y++) {
+        for (let z = 0; z < amount; z++) {
+          dummy.position.set(offset - x, offset - y, offset - z)
+          dummy.rotation.y =
+            Math.sin(x / 2 + time) +
+            Math.sin(y / 3 + time) +
+            Math.sin(z / 4 + time)
+          dummy.rotation.z = dummy.rotation.y * 2
+
+          dummy.updateMatrix()
+
+          meshRef.current.setMatrixAt(i++, dummy.matrix)
+        }
+        meshRef.current.instanceMatrix.needsUpdate = true
+      }
+    }
+  })
+
+  return (
+    <instancedMesh ref={meshRef} args={[null, null, 1000]}>
+      <torusGeometry args={[6, 0.1, 32, 32]}></torusGeometry>
+      <meshPhongMaterial color={color} />
+    </instancedMesh>
+  )
+}
+
+const CameraControls = () => {
+  const {
+    camera,
+    gl: { domElement },
+  } = useThree()
+  // Ref to the controls, so that we can update them on every frame using useFrame
+  const controls = useRef(null)
+  useFrame((state) => controls.current.update())
+  return <orbitControls ref={controls} args={[camera, domElement]} />
+}
+
+const DEFAULT_ANALYSIS = {
+  mood: '',
+  summary: '',
+  color: '049ef4',
+  subject: '',
+  negative: false,
+}
 
 const Home = memo(function HomePage() {
-  const { getToken, isLoaded, isSignedIn } = useAuth()
-  const renderTarget = useRef(null)
-  useEffect(() => {
-    let continueRendering = true
-    const scene = new THREE.Scene()
-    scene.background = new THREE.Color(0xffffff)
-    const camera = new THREE.PerspectiveCamera(
-      75,
-      renderTarget.current.clientWidth / renderTarget.current.clientHeight,
-      0.1,
-      1000
-    )
-    const renderer = new THREE.WebGLRenderer()
-    renderer.setSize(
-      renderTarget.current.clientWidth,
-      renderTarget.current.clientHeight
-    )
-    renderTarget.current.replaceChildren(renderer.domElement)
-    const geometry = new THREE.TorusKnotGeometry(50, 20, 50, 16)
-    const material = new THREE.MeshBasicMaterial({
-      color: 0x049ef4,
-      wireframe: true,
-    })
-    const torusKnot = new THREE.Mesh(geometry, material)
-    scene.add(torusKnot)
-    const directionalLight = new THREE.DirectionalLight(0x000000, 0.5)
-    scene.add(directionalLight)
-    const light = new THREE.AmbientLight(0x404040)
-    scene.add(light)
-    camera.position.z = 200
-    function animate() {
-      if (continueRendering) {
-        requestAnimationFrame(animate)
-      }
-      torusKnot.rotation.x += 0.01
-      torusKnot.rotation.y += 0.01
-      renderer.render(scene, camera)
-    }
-    animate()
-    return () => {
-      continueRendering = false
-    }
-  }, [])
+  const { isSignedIn } = useAuth()
+  const [textAreaContent, setTextareaContent] = useState('')
+  const [showForm, setShowForm] = useState(false)
+  const [analysis, setAnalysis] = useState(DEFAULT_ANALYSIS)
+  const [requestsLeft, setRequestsLeft] = useState()
+  const { mood, summary, color } = analysis || DEFAULT_ANALYSIS
+  const [isLoading, setIsLoading] = useState(false)
 
   let href = '/'
   if (isSignedIn) {
@@ -67,15 +96,86 @@ const Home = memo(function HomePage() {
             analysing your daily entries.
           </p>
           <div>
-            <Link href={href}>
-              <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl text-white">
-                Get started
+            <div
+              className={classNames({
+                hidden: showForm,
+              })}
+            >
+              {isLoading && (
+                <div className="flex">
+                  <FaSpinner className="animate-spin" />
+                  <span className="mx-1">Loading</span>
+                </div>
+              )}
+              <textarea
+                value={textAreaContent}
+                onChange={(e) => {
+                  setTextareaContent(e.currentTarget.value)
+                }}
+                placeholder="What is on your mind today ?"
+                className="w-full p-3 border border-solid rounded border-sky-500"
+              />
+              <div
+                className={classNames('text-xs text-black/60', {
+                  hidden: requestsLeft == null,
+                })}
+              >
+                Remaining requests - {requestsLeft}. Login to save your entries
+                and view trends over time.
+              </div>
+              <button
+                className={classNames(
+                  'bg-green-600 px-4 py-2 rounded-lg text-xl text-white my-2',
+                  {
+                    hidden: requestsLeft == 0,
+                  }
+                )}
+                onClick={async () => {
+                  setIsLoading(true)
+                  const { ai, left, error } = await requestAnalysis(
+                    textAreaContent
+                  )
+                  if (ai) {
+                    setAnalysis(ai)
+                  }
+                  setRequestsLeft(left)
+                  setIsLoading(false)
+                }}
+                disabled={isLoading || textAreaContent.length === 0}
+              >
+                Submit
+              </button>
+              {mood && (
+                <div className="text-xl text-black/60 m-2">
+                  {mood.toLocaleUpperCase()}
+                </div>
+              )}
+              {summary && (
+                <div className="text-lg text-black/60 m-2">{summary}</div>
+              )}
+            </div>
+            <Link
+              href={href}
+              className={classNames({
+                hidden:
+                  requestsLeft == null || (requestsLeft && requestsLeft > 0),
+              })}
+            >
+              <button className="bg-blue-600 px-4 py-2 my-4 rounded-lg text-xl text-white">
+                {isSignedIn ? 'Get started' : 'Login'}
               </button>
             </Link>
           </div>
         </div>
       </div>
-      <div ref={renderTarget}></div>
+      <div>
+        <Canvas camera={{ position: [0, 20, 5] }}>
+          <ambientLight intensity={1} />
+          <directionalLight />
+          <Cubes color={color} />
+          <CameraControls />
+        </Canvas>
+      </div>
     </div>
   )
 })
