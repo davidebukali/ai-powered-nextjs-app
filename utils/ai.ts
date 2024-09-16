672c7e2965645f4191e10baa4f6629ac0cb25dd7
David <debukali@gmail.com>
2024-09-17 00:24:01 +0300

    Create rag prompt template

--- utils/ai.ts
+++ utils/ai.ts
@@ -2,8 +2,13 @@ const { GoogleGenerativeAI } = require('@google/generative-ai')
 import { StructuredOutputParser } from 'langchain/output_parsers'
 import z from 'zod'
 import { PromptTemplate } from '@langchain/core/prompts'
+import { log } from 'console'
 
 const genAI = new GoogleGenerativeAI(process.env.API_KEY)
+const semanticAnalysisTemplate =
+  'Analyze the following journal entry. Follow the intructions and format your response to match the format instructions, no matter what! \n{formatted_instructions}\n{entry}'
+const ragTemplate =
+  'You are a helpful assistant. You will be given context and a question. Your task is to answer the question based only on the provided context. Do not use any external knowledge or make assumptions. You can use synonyms when refering to the context in your responses or occasionally leave it out of your responses. \nContext:\n{context}\nQuery:{query}'
 export const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' })
 
 const parser = StructuredOutputParser.fromZodSchema(
@@ -21,7 +26,7 @@ const parser = StructuredOutputParser.fromZodSchema(
     negative: z
       .boolean()
       .describe(
-        'is the journal entry negative ? (i.e does it containe negative emotions?).'
+        'is the journal entry negative ? (i.e does it contain negative emotions?).'
       ),
     color: z
       .string()
@@ -31,30 +36,57 @@ const parser = StructuredOutputParser.fromZodSchema(
   })
 )
 
-const getPrompt = async (content: string) => {
-  const formatted_instructions = parser.getFormatInstructions()
-  const prompt = new PromptTemplate({
-    template:
-      'Analyze the following journal entry. Follow the intructions and format your response to match the format instructions, no matter what! \n{formatted_instructions}\n{entry}',
-    inputVariables: ['entry'],
-    partialVariables: { formatted_instructions },
-  })
+// ragTemplate requires content parameter to have context and query variables
+// semanticAnalysisTemplate requires just entry to be included in the content parameter
+const getPrompt = async (
+  template: string,
+  content: { entry: string } | { context: string; query: string },
+  formatted_instructions?: string
+) => {
+  const promptData = {
+    template,
+    inputVariables: formatted_instructions ? ['entry'] : ['context', 'query'],
+    ...(formatted_instructions && {
+      partialVariables: { formatted_instructions },
+    }),
+  }
+  const prompt = new PromptTemplate(promptData)
 
-  const input = await prompt.format({
-    entry: content,
-  })
-
-  return input
+  return await prompt.format(content)
 }
 
 export const analyze = async (content: string) => {
-  const input = await getPrompt(content)
+  const input = await getPrompt(
+    semanticAnalysisTemplate,
+    {
+      entry: content,
+    },
+    parser.getFormatInstructions()
+  )
   const result = await model.generateContent(input)
   const response = await result.response
   const text = response.text()
 
   try {
     return parser.parse(text)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+export const ragQuery = async (query: string, context: string) => {
+  const input = await getPrompt(ragTemplate, {
+    query,
+    context,
+  })
+  const result = await model.generateContent(input)
+
+  const response = await result.response
+
+  const text = response.text()
+
+  try {
+    return text
   } catch (error) {
     console.log(error)
   }
